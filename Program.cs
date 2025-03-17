11b98e4df6f771ceab92b7ac3eeaeb19af37f8ce
ksenera <ksenera7130@conestogac.on.ca>
2025-03-16 23:30:47 -0400

    fixed the namespace declaration issue

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
 if (builder.Environment.IsDevelopment())
 {
--- obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f22ccbf69a0101a09d8b11e0e13f6322968322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0db822d12cd0173d1637f24bd9f6afd13f08242")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
